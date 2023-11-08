ee033a702f234305777282983ad22184b7d97332
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2023-11-08 10:07:48 +0000

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server'
 
-// Error: Invariant: no match found for request.
-// Pathname '/api/wasm' should have matched '/api/edge-geo'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
 export const config = {
     matcher: ['/api/wasm'],
 }
 
-export const middleware = (req: NextRequest) => {
-    req.nextUrl.pathname = `/api/edge-geo`
-    return NextResponse.rewrite(req.nextUrl)
+// Error: Invariant: no match found for request.
+// Pathname '/api/wasm' should have matched '/api/edge-geo'
+// TODO: rewrite will error, but redirect can work
+// export const middleware = (request: NextRequest) => {
+//     return NextResponse.rewrite(new URL(`/api/edge-geo`, request.url)) 
+// }
+
+export const middleware = (request: NextRequest) => {
+    return NextResponse.redirect(new URL(`/api/edge-geo`, request.url))
 }
