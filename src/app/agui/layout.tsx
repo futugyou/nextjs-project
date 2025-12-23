dae0897022846b49d1c94be222918b45a031d4cd
futugyou <17871902+futugyou@users.noreply.github.com>
2025-12-23 20:28:17 +0000

    Updated agui layout

--- src/app/agui/layout.tsx
+++ src/app/agui/layout.tsx
@@ -1,10 +1,20 @@
-import { CopilotKit } from "@copilotkit/react-core"; 
+"use client";
+
+import { usePathname } from 'next/navigation';
+
+import { CopilotKit } from "@copilotkit/react-core";
 import "@copilotkit/react-ui/styles.css";
 
-export default function RootLayout({ children }: {children: React.ReactNode}) {
-  return ( 
-        <CopilotKit runtimeUrl="/api/copilotkit/light" agent="light">
-          {children}
-        </CopilotKit> 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const pathSegment = pathname.split('/')[2];
+
+  return (
+    <CopilotKit
+      runtimeUrl={`/api/copilotkit/${pathSegment}`}
+      agent={pathSegment}
+    >
+      {children}
+    </CopilotKit>
   );
 }
