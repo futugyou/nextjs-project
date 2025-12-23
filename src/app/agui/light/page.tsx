4d9e7c32494e9607ece125ffeb85100b77d297fa
futugyou <17871902+futugyou@users.noreply.github.com>
2025-12-23 20:38:44 +0000

    Use CopilotChat instead of CopilotSidebar

--- src/app/agui/light/page.tsx
+++ src/app/agui/light/page.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { CopilotSidebar } from "@copilotkit/react-ui";
+import { CopilotChat, CopilotSidebar } from "@copilotkit/react-ui";
 
 export default function Page() {
   return (
-    <main>
-      <CopilotSidebar
-        labels={{
-          title: "Your Assistant",
-          initial: "Hi! How can I help you today?",
-        }}
-      />
-      <h1>Your App</h1>
-    </main>
+    <div
+      className="flex justify-center items-center h-full w-full"
+      data-testid="background-container"
+    >
+      <div className="h-full w-full md:w-8/10 md:h-8/10 rounded-lg">
+        <CopilotChat
+          className="h-full rounded-2xl max-w-6xl mx-auto"
+          labels={{ initial: "Hi, I'm an agent. Want to chat?" }}
+          suggestions={[
+            {
+              title: "get light`s states",
+              message: "Get the status of all the lights.",
+            },
+            {
+              title: "Turn off the lights and go to sleep.",
+              message: "Turn off all the lights.",
+            },
+          ]}
+        />
+      </div>
+    </div>
   );
 }
