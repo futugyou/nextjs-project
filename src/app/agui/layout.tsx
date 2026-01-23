67b63af36586d9af9b212f25a0b98ef321827df8
futugyou <17871902+futugyou@users.noreply.github.com>
2026-01-23 14:59:13 +0000

    Added agui publicLicenseKey, and css

--- src/app/agui/layout.tsx
+++ src/app/agui/layout.tsx
@@ -1,23 +1,26 @@
-"use client";
+"use client"
 
-import { usePathname } from 'next/navigation';
+import "./style.css"
+import "@copilotkit/react-ui/styles.css"
 
-import { CopilotKit } from "@copilotkit/react-core";
-import "@copilotkit/react-ui/styles.css";
-import CopilotStatus from "@/components/CopilotStatus";
+import { usePathname } from 'next/navigation'
+
+import { CopilotKit } from "@copilotkit/react-core"
+import CopilotStatus from "@/components/CopilotStatus"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const pathSegment = pathname.split('/')[2];
+  const pathname = usePathname()
+  const pathSegment = pathname.split('/')[2]
 
   return (
     <CopilotKit
       runtimeUrl={`/api/copilotkit/${pathSegment}`}
       agent={pathSegment}
+      publicLicenseKey={process.env.NEXT_PUBLIC_COPILOTKIT_LICENSE_KEY}
     >
       {/* TODO: It will report a "Hydration failed" error, so I've commented it out for now. */}
       {/* <CopilotStatus agentId={pathSegment}></CopilotStatus> */}
       {children}
     </CopilotKit>
-  );
+  )
 }
