491515a8ca24e8d550d766a573ec543129be8456
futugyou <17871902+futugyou@users.noreply.github.com>
2026-03-16 23:23:28 +0800

    Updated middleware

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const SUPPORTED_LOCALES = ['zh', 'en']
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+
+  if (pathname === '/api/wasm') {
+    return NextResponse.redirect(new URL(`/api/edge-geo`, request.url))
+  }
+
+  const pathnameLocale = SUPPORTED_LOCALES.find(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  )
+
+  if (pathnameLocale) {
+    let targetPath = pathname.replace(new RegExp(`^/${pathnameLocale}`), '')
+
+    if (!targetPath.startsWith('/')) {
+      targetPath = '/' + targetPath
+    }
+
+    const url = request.nextUrl.clone()
+    url.pathname = targetPath
+
+    const response = NextResponse.rewrite(url)
+    response.cookies.set('NEXT_LOCALE', pathnameLocale)
+
+    return response
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/api/wasm', '/((?!api|_next|.*\\..*).*)'],
+}
